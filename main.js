364d48f4b755c756505a4367a9c9a905e6ef8099
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2019-05-11 16:48:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
+const body = document.body
 
-ScrollReveal().reveal('.scroll_reveal');
-ScrollReveal().reveal('.scroll_reveal li');
 
+document.onreadystatechange = function () {
+    if (document.readyState == "complete") {
+
+        body.style.transition = 'opacity 1s cubic-bezier(0, 0, 0.2, 1)';
+        body.style.opacity = 1;
+        setTimeout(() => {
+            body.style.transition = 'unset';
+        }, 1000);
+    }
+}
