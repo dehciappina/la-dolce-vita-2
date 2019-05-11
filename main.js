9152014bd7a425e80feafcfa45441e88ca3ab487
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2019-05-11 16:36:05 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,36 +1,4 @@
-const body = document.body
+
 ScrollReveal().reveal('.scroll_reveal');
 ScrollReveal().reveal('.scroll_reveal li');
 
-document.onreadystatechange = function () {
-    if (document.readyState == "complete") {
-
-        setTimeout(() => {
-            body.style.transition = 'opacity 1s cubic-bezier(0, 0, 0.2, 1)';
-            body.style.opacity = 1;
-            // body.style.transition = 'unset';
-        }, 1000);
-
-        // loadjscssfile("fold.css", "css")
-        
-    }
-
-    function loadjscssfile(filename, filetype){
-        if (filetype=="js"){ //if filename is a external JavaScript file
-            var fileref=document.createElement('script')
-            fileref.setAttribute("type","text/javascript")
-            fileref.setAttribute("src", filename)
-        }
-        else if (filetype=="css"){ //if filename is an external CSS file
-            var fileref=document.createElement("link")
-            fileref.setAttribute("rel", "stylesheet")
-            fileref.setAttribute("type", "text/css")
-            fileref.setAttribute("href", filename)
-        }
-        if (typeof fileref!="undefined") {
-            document.getElementsByTagName("head")[0].appendChild(fileref)
-        }
-    }
-    
-    
-}
