e1fed5660ad98f751caa33d3e0b00daa6a9f12f7
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2019-05-14 16:35:05 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-const body = document.body
+const body = document.body;
 
+    // IF MOBILE
+if((typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1)) {
+} else {
+    document.querySelector('#salgados_imgs').innerHTML = '<img src="assets/coxinha.jpg" alt=""><img src="assets/empada.jpg" alt=""><img src="assets/kibe.jpg" alt=""><img src="assets/torta1.jpg" alt=""><img src="assets/coxinha.jpg" alt="">';
+    console.log('desktop')
+}
 
 document.onreadystatechange = function () {
     if (document.readyState == "complete") {
-
         body.style.transition = 'opacity 1s cubic-bezier(0, 0, 0.2, 1)';
         body.style.opacity = 1;
         setTimeout(() => {
